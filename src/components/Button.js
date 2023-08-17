55bc20130c391a8f8104e497f1518ca10b75c84e
danny-mak1993 <danny.mak2@gmail.com>
2023-08-16 20:42:04 -0500

    adjusted code to make navbar work

--- src/components/Button.js
+++ src/components/Button.js
@@ -17,7 +17,7 @@ export const Button = ({
         ? buttonStyle
         : STYLES[0];
 
-    const checkButtonSize = SIZES / includes(buttonSize)
+    const checkButtonSize = SIZES.includes(buttonSize)
         ? buttonSize
         : SIZES[0];
 

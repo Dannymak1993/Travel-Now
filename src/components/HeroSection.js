1f98431d40be816612c71793ec9f0068260fc726
danny-mak1993 <danny.mak2@gmail.com>
2023-08-22 13:29:08 -0500

    added second button to hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -17,6 +17,14 @@ function HeroSection() {
         >
           GET STARTED
         </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+        >
+          WATCH TRAILER <i className='far 
+          fa-play-circle' />
+        </Button>
       </div>
     </div>
   )

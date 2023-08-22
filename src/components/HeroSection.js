7def689bfd59b6444c4cb479396a5df9899dfd26
danny-mak1993 <danny.mak2@gmail.com>
2023-08-22 13:21:55 -0500

    added video to hero component

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function HeroSection() {
   return (
-    <div>HeroSection</div>
+    <div className='hero-container'>
+      <video src="/videos/video-2.mp4" autoPlay loop muted/>
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      
+    </div>
   )
 }
 

ee7dc3d99cfd979d74f813785537072ac112f974
danny-mak1993 <danny.mak2@gmail.com>
2023-08-21 20:50:36 -0500

    exported home component

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -3,3 +3,15 @@ import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
+
+function Home() {
+    return (
+        <>
+            <HeroSection />
+            <Cards />
+            <Footer />
+        </>
+    );
+}
+
+export default Home;

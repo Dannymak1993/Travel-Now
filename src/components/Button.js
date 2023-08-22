05731067827d0274d06c1242f9ffdc2ae1b62831
danny-mak1993 <danny.mak2@gmail.com>
2023-08-22 13:42:55 -0500

    added cards.css

--- src/components/Button.js
+++ src/components/Button.js
@@ -17,19 +17,17 @@ export const Button = ({
         ? buttonStyle
         : STYLES[0];
 
-    const checkButtonSize = SIZES.includes(buttonSize)
-        ? buttonSize
-        : SIZES[0];
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
     return (
         <Link to='/sign-up' className='btn-mobile'>
-            <button 
-            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-            onClick={onClick}
-            type={type}
+            <button
+                className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+                onClick={onClick}
+                type={type}
             >
                 {children}
             </button>
         </Link>
-    )
+    );
 };

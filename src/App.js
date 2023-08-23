a490444ada38b620f293bc131819cc57cbc0f863
danny-mak1993 <danny.mak2@gmail.com>
2023-08-22 23:58:07 -0500

    changing some paths to see if it fixes the problem

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/Travel-Now' element={<Home />} />
         <Route path='/services' element={<Services />} />
         <Route path='/products' element={<Products />} />
         <Route path='/sign-up' element={<SignUp />} />
